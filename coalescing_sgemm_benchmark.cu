8b35d8d7209b82e303a15f2310409b8d3490b965
Jiajun Sun <jiajunstanley@gmail.com>
2026-01-07 07:33:15 +0000

    fix a bug and add coalescing gemm

--- coalescing_sgemm_benchmark.cu
+++ coalescing_sgemm_benchmark.cu
@@ -14,11 +14,14 @@
     } \
 }
 
-__global__ void naive_sgemm(int M, int N, int K, float alpha, const float* A,
-                            const float* B, float beta, float* C) {
+__global__ void coalescing_sgemm(int M, int N, int K, float alpha, const float* A,
+                                 const float* B, float beta, float* C) {
     // compute position in C that this thread will compute
-    const uint x = blockIdx.x * blockDim.x + threadIdx.x;
-    const uint y = blockIdx.y * blockDim.y + threadIdx.y;
+    // const uint x = blockIdx.x * blockDim.x + threadIdx.x;
+    // const uint y = blockIdx.y * blockDim.y + threadIdx.y;
+
+    const uint x = blockIdx.y * blockDim.y + threadIdx.y;
+    const uint y = blockIdx.x * blockDim.x + threadIdx.x;
 
     // if condition is necessary if matrix dimensions are not multiples of block size
     if (x < N && y < M) {
@@ -27,7 +30,7 @@ __global__ void naive_sgemm(int M, int N, int K, float alpha, const float* A,
             sum += A[x * K + k] * B[k * N + y];
         }
 
-        C[y * N + x] = alpha * sum + beta * C[y * N + x];
+        C[x * N + y] = alpha * sum + beta * C[x * N + y];
     }                           
 }
 
@@ -40,7 +43,7 @@ int main() {
     float alpha = 1.0f;
     float beta = 0.0f;
 
-    printf("Benchmarking Naive SGEMM [M=%d, N=%d, K=%d]\n", M, N, K);
+    printf("Benchmarking Coalescing SGEMM [M=%d, N=%d, K=%d]\n", M, N, K);
 
     // Grid config: x covers N (cols), y covers M (rows) to match kernel checks
     dim3 gridDim(CEIL_DIV(N, 32), CEIL_DIV(M, 32), 1);
@@ -63,7 +66,7 @@ int main() {
     CHECK_CUDA(cudaMemset(d_C, 0, size_C));
 
     // 4. Warm-up
-    naive_sgemm<<<gridDim, blockDim>>>(M, N, K, alpha, d_A, d_B, beta, d_C);
+    coalescing_sgemm<<<gridDim, blockDim>>>(M, N, K, alpha, d_A, d_B, beta, d_C);
     CHECK_CUDA(cudaGetLastError());
     CHECK_CUDA(cudaDeviceSynchronize());
 
@@ -76,7 +79,7 @@ int main() {
 
     CHECK_CUDA(cudaEventRecord(start));
     for (int i = 0; i < n_iter; i++) {
-        naive_sgemm<<<gridDim, blockDim>>>(M, N, K, alpha, d_A, d_B, beta, d_C);
+        coalescing_sgemm<<<gridDim, blockDim>>>(M, N, K, alpha, d_A, d_B, beta, d_C);
     }
     CHECK_CUDA(cudaEventRecord(stop));
     CHECK_CUDA(cudaEventSynchronize(stop));
--- run.sh
+++ run.sh
@@ -2,5 +2,7 @@ mkdir -p build
 
 nvcc -O3 -arch=sm_80 -lcublas cublas_benchmark.cu -o build/cublas_benchmark
 nvcc -O3 -arch=sm_80 -lcublas naive_sgemm_benchmark.cu -o build/naive_sgemm_benchmark
+nvcc -O3 -arch=sm_80 -lcublas coalescing_sgemm_benchmark.cu -o build/coalescing_sgemm_benchmark
 ./build/cublas_benchmark
 ./build/naive_sgemm_benchmark
+./build/coalescing_sgemm_benchmark
